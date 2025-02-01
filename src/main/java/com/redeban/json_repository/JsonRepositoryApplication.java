840c258c8b8cdb0398e9e69b67f4454cd8aa0d31
Andres Castillo <elidibs90@gmail.com>
2025-01-31 20:17:04 -0500

    Initial commit

---
+++ src/main/java/com/redeban/json_repository/JsonRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.redeban.json_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redeban/json_repository/JsonRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeban.json_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
