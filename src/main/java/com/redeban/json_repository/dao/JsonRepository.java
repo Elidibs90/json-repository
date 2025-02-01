219856dc331b056f4a64579589908ff9139c6543
Andres Castillo <elidibs90@gmail.com>
2025-01-31 21:27:13 -0500

    Add getters and setters

--- src/main/java/com/redeban/json_repository/dao/JsonRepository.java
+++ src/main/java/com/redeban/json_repository/dao/JsonRepository.java
@@ -19,5 +19,27 @@ public class JsonRepository {
     @JdbcTypeCode(SqlTypes.JSON)
     private MyJson jsonProperty;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public MyJson getJsonProperty() {
+        return jsonProperty;
+    }
+
+    public void setJsonProperty(MyJson jsonProperty) {
+        this.jsonProperty = jsonProperty;
+    }
 }
